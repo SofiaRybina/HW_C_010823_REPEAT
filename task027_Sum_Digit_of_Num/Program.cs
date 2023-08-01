1c044763518671842bc5e8c83aedfb5899715ec4
Sofia <rybina.sofia@gmail.com>
2023-08-01 21:54:24 +0300

    Сумма цифр в числе

---
+++ task027_Sum_Digit_of_Num/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int readInput = Convert.ToInt32(Console.ReadLine());
+    int result = Math.Abs(readInput);
+    return result;
+}
+
+int DigitSum(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10; //result = result + (number % 10);
+        number /=10; // number = number / 10;
+    }
+    return result;
+}
+
+int num = Prompt("Input number: ");
+Console.WriteLine();
+
+Console.WriteLine($"Number={num} DigitSum = {DigitSum(num)}");
